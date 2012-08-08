7f420a99e4a4c54c8a05dbb6f12607267c4cc631
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-08-08 12:58:52 +1000

    Improved toString output and Shiro required responses in FederatedToken.

--- src/groovy/grails/plugins/federatedgrails/FederatedToken.groovy
+++ src/groovy/grails/plugins/federatedgrails/FederatedToken.groovy
@@ -2,13 +2,18 @@ package grails.plugins.federatedgrails
 
 public class FederatedToken implements org.apache.shiro.authc.AuthenticationToken {
 
-    def principal, credential, attributes, remoteHost, userAgent
+  def principal, credential, attributes, remoteHost, userAgent
 
-  public Object getPrincipal() {
-      return this.principal
+  public Object getCredentials() {
+    return this.credential
   }
   
-    public Object getCredentials() {
-      return null
+  public Object getPrincipal() {
+    return this.principal
   }
+
+  public String toString() {
+    "grails.plugins.federatedgrails.FederatedToken [principal:$principal, credential:$credential, attributes:$attributes, remoteHost:$remoteHost, userAgent:$userAgent]"
+  }
+  
 }
